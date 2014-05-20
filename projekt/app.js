71137a276e1422ceebe5ad4f99b5757136636b09
waveq <nynonn@gmail.com>
2014-05-20 18:22:53 +0200

    działa logowanie, nie sprawdza czy użytkownik jest zalogowany

--- projekt/app.js
+++ projekt/app.js
@@ -6,6 +6,50 @@ var httpServer = require("http").createServer(app);
 var socketio = require("socket.io");
 var io = socketio.listen(httpServer);
 
+// XXX 
+var connect = require('connect');
+var sessionSecret = 'wielkiSekret44';
+var sessionKey = 'connect.sid';
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var passportSocketIo = require('passport.socketio');
+var sessionStore = new connect.session.MemoryStore();
+
+// Konfiguracja passport.js
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        if ((username === 'admin') && (password === 'tajne')) {
+            console.log("Udane logowanie...");
+            return done(null, {
+                username: username,
+                password: password
+            });
+        } else {
+            return done(null, false);
+        }
+    }
+));
+
+app.use(express.cookieParser());
+app.use(express.urlencoded());
+app.use(express.session({
+    store: sessionStore,
+    key: sessionKey,
+    secret: sessionSecret
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 var history = [];
 
 app.use(express.static("public"));
@@ -16,13 +60,23 @@ io.sockets.on('connection', function (socket) {
 		history.unshift(data);
 		io.sockets.emit('rec msg', data);
 	});
-	socket.emit('history', history);
 });
 
 app.get('/login', function (req, res) {
 	var loginPage = "public/login.html";
 	res.sendfile(loginPage, {root: __dirname })
 });
+
+
+// LOGOWANIE
+app.post('/login',
+    passport.authenticate('local', {
+        failureRedirect: '/login'
+    }),
+    function (req, res) {
+        res.redirect('/');
+    }
+);
 
 httpServer.listen(3000, function () {
     console.log('Serwer HTTP działa na pocie 3000');
